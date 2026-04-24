c59ebd3eba7872e7e08b3f646cac8ed93534e790
Yeremi-PD <yeremipd353@gmail.com>
2026-04-24 15:37:24 -0400

    A

--- app.py
+++ app.py
@@ -12,7 +12,7 @@ CATEGORIAS = [
 
 # --- CONEXIÓN A GOOGLE SHEETS ---
 # Pega aquí la URL de tu Google Sheet
-URL_GOOGLE_SHEET = "PON_AQUI_LA_URL_DE_TU_GOOGLE_SHEET_COMPLETA"
+URL_GOOGLE_SHEET = "https://docs.google.com/spreadsheets/d/1abc123xyz456..."
 
 # Establecer conexión
 conn = st.connection("gsheets", type=GSheetsConnection)
